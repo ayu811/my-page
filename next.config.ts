910c2346d8a0e8a985f54ed5343002cae14159d3
ayu811 <ayuhaya.create@gmail.com>
2025-02-25 22:49:09 +0900

    some fixed

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  basePath: "my-page"
+  basePath: "/my-page"
 };
 
 export default nextConfig;
