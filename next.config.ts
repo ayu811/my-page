964a9f1ce310399945ef2c4dcb99d7da25c0840a
ayu811 <ayuhaya.create@gmail.com>
2025-02-25 22:59:23 +0900

    some edits

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,13 @@
 import type { NextConfig } from "next";
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = "";
-let basePath = "/my-page"; // basePath は既に設定されているのでそのまま
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-  assetPrefix = `/${repo}`; // assetPrefix を設定
-   //basePath = `/${repo}`; リポジトリ名とbasePathが異なる場合は必要
-}
-
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
-  assetPrefix: assetPrefix,
-  basePath: basePath,
+  output: 'export',
+  assetPrefix: '/my-page',
+  basePath: 'my-page',
+  trailingSlash: true,
   images: {
-      unoptimized: true,
+    unoptimized: true,
   },
 };
 
